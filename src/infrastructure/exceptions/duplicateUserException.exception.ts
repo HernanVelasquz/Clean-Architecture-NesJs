5af25d44756cdfc699d90dcb8210eb50977eeb95
Hernan Velasquez <velasquezmhernan@gmail.com>
2023-06-21 16:35:23 -0500

    docs: :memo: Documentando Excepciones JSDocs
    Documentando Excepciones bajo el estandar JSDocs

--- src/infrastructure/exceptions/duplicateUserException.exception.ts
+++ src/infrastructure/exceptions/duplicateUserException.exception.ts
@@ -1,6 +1,15 @@
 import { ConflictException } from '@nestjs/common';
 
+/**
+ * @class DuplicateUserException
+ * @classdesc Excepción personalizada para indicar un conflicto debido a un usuario duplicado.
+ * @extends ConflictException
+ */
 export class DuplicateUserException extends ConflictException {
+  /**
+   * @constructor
+   * @param {string} email - El email del usuario duplicado.
+   */
   constructor(email: string) {
     super(
       `El usuario con ${email} ya se encuentra registrado en la base de datos`,
--- src/infrastructure/exceptions/insufficientFundsException.exception.ts
+++ src/infrastructure/exceptions/insufficientFundsException.exception.ts
@@ -1,6 +1,14 @@
 import { BadRequestException } from '@nestjs/common';
 
+/**
+ * @class InsufficientFundsException
+ * @classdesc Excepción personalizada para indicar un error de fondos insuficientes en la cuenta.
+ * @extends BadRequestException
+ */
 export class InsufficientFundsException extends BadRequestException {
+  /**
+   * @constructor
+   */
   constructor() {
     super('Fondos insuficientes en la cuenta');
   }
