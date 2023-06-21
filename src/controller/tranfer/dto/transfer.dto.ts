90ec493de99840e1744893132e87cf3458a52005
Hernan Velasquez <velasquezmhernan@gmail.com>
2023-06-21 16:57:05 -0500

    docs: :memo: Agregando documentacion JsDocs  Transfer
    Agregando documentacion bajo el estandar JsDocs para los endpoints de transferencia

--- src/controller/tranfer/dto/transfer.dto.ts
+++ src/controller/tranfer/dto/transfer.dto.ts
@@ -1,15 +1,32 @@
 import { IsEmail, IsNotEmpty, IsPositive } from 'class-validator';
 import { UserEntity } from 'src/domain';
 
+/**
+ * @class TransferDto
+ * @classdesc DTO para transferencias de fondos entre usuarios.
+ * @extends UserEntity
+ */
 export class TransferDto extends UserEntity {
+  /**
+   * Correo electrónico del destinatario de la transferencia.
+   * @member {string}
+   */
   @IsEmail()
   @IsNotEmpty()
   toEmail: string;
 
+  /**
+   * Correo electrónico del remitente de la transferencia.
+   * @member {string}
+   */
   @IsEmail()
   @IsNotEmpty()
   fromEmail: string;
 
+  /**
+   * Valor de la transferencia.
+   * @member {number}
+   */
   @IsPositive()
   @IsNotEmpty()
   valueTransfer: number;
--- src/controller/tranfer/transfer.controller.ts
+++ src/controller/tranfer/transfer.controller.ts
@@ -10,6 +10,12 @@ import {
 import { TransferEntity } from 'src/domain';
 import { TransferDto } from './dto';
 
+/**
+ * @class TransferController
+ * @classdesc Controlador para las operaciones relacionadas con transferencias de fondos.
+ * @param {RegisterTransferUseCase} registerTransferUseCase - Instancia de RegisterTransferUseCase para realizar el registro de transferencias.
+ * @param {GetHistoryTransferUseCase} getHistoryTransferUseCase - Instancia de GetHistoryTransferUseCase para obtener el historial de transferencias.
+ */
 @UseGuards(AuthGuard('jwt'))
 @Controller('transfer')
 export class TransferController {
@@ -18,6 +24,12 @@ export class TransferController {
     private readonly getHistoryTransferUseCase: GetHistoryTransferUseCase,
   ) {}
 
+  /**
+   * Registra una nueva transferencia de fondos.
+   * @method registerTransfer
+   * @param {TransferDto} createUserDto - DTO de transferencia que contiene la información de la transferencia.
+   * @returns {Observable<TransferEntity>} Observable que emite la entidad de transferencia registrada.
+   */
   @Post()
   registerTransfer(
     @Body() createUserDto: TransferDto,
@@ -25,8 +37,17 @@ export class TransferController {
     return from(this.registerTransferUseCase.registerTransfer(createUserDto));
   }
 
+  /**
+   * Obtiene el historial de transferencias de un cliente específico.
+   * @method getHistoryTransfer
+   * @param {string} client_id - ID del cliente para el cual se desea obtener el historial de transferencias.
+   * @returns {Observable<TransferEntity[]>} Observable que emite un array de entidades de transferencia correspondientes al historial del cliente.
+   */
+
   @Get(':client_id')
-  getHistoryTransfer(@Param('client_id') client_id: string): Observable<any> {
+  getHistoryTransfer(
+    @Param('client_id') client_id: string,
+  ): Observable<TransferEntity[]> {
     return from(
       this.getHistoryTransferUseCase.historyTransferUseCase(client_id),
     );
